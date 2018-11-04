daf36e466caa8e9a543cb254b46f8e466b19336c
Ken Sodemann <ken@ionic.io>
2018-11-04 07:28:43 -0600

    WIP - improve layout

--- src/app/ballot/ballot.page.spec.ts
+++ src/app/ballot/ballot.page.spec.ts
@@ -1,18 +1,27 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { BallotPage } from './ballot.page';
+import {
+  CarShowsService,
+  createCarShowsServiceMock,
+  testCarShows
+} from '../services/car-shows';
 
 describe('BallotPage', () => {
   let component: BallotPage;
   let fixture: ComponentFixture<BallotPage>;
+  let carShows;
 
   beforeEach(async(() => {
+    carShows = createCarShowsServiceMock();
+    carShows.getCurrent.and.returnValue(of(testCarShows[1]));
     TestBed.configureTestingModule({
-      declarations: [ BallotPage ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+      declarations: [BallotPage],
+      providers: [{ provide: CarShowsService, useValue: carShows }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,7 +30,16 @@ describe('BallotPage', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('exists', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('initialization', () => {
+    it('gets the current car show', () => {
+      expect(carShows.getCurrent).toHaveBeenCalledTimes(1);
+      expect(component.carShow).toEqual(testCarShows[1]);
+    });
+  });
+
+  describe('saving', () => {});
 });
--- src/app/ballot/ballot.page.ts
+++ src/app/ballot/ballot.page.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CarShow } from '../models/car-show';
+import { CarShowsService } from '../services/car-shows';
+
 @Component({
   selector: 'app-ballot',
   templateUrl: './ballot.page.html',
   styleUrls: ['./ballot.page.scss']
 })
 export class BallotPage implements OnInit {
-  constructor() {}
+  carShow: CarShow;
 
-  ngOnInit() {}
+  constructor(private carShows: CarShowsService) {}
+
+  ngOnInit() {
+    this.carShows.getCurrent().subscribe(c => (this.carShow = c));
+  }
+
+  testClick() {
+    console.log('back button clicked');
+  }
 }
